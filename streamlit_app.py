d38ebc32bfa6a897d2d663733e5edfed106e839b
Anastatia <63119392+AnastatiaDorval@users.noreply.github.com>
2022-09-15 10:36:41 -0500

    Update streamlit_app.py
    added new fruityvice section and json text

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #displaying list on page
 streamlit.dataframe(fruits_to_show)
-#displaying fruityvice api response
+#new section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
