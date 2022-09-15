1ea64723207f383341277bda0bc4a8d86e8f882d
Anastatia <63119392+AnastatiaDorval@users.noreply.github.com>
2022-09-15 10:31:55 -0500

    Update streamlit_app.py
    adding fruity vice text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Moms New Healthy Diner')
 
@@ -20,3 +21,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #displaying list on page
 streamlit.dataframe(fruits_to_show)
+#displaying fruityvice api response
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
